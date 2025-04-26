f95334b579e3fb6b786b6df501c7fba8d0a33a56
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-04-26 22:22:05 +0600

    include fles done

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?
+
+$DB_HOST = 'localhost'; // Database host
+$DB_USER = 'root'; // Database user
+$DB_PASS = 'arka'; // Database password
+$DB_NAME = 'photo_gallery'; // Database name
+
+try {
+    // Create a new PDO instance
+    $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoY9CUG9s4M4dksO7r1LRDIv3Q1w1Rr58+7tx0x04k2Jp3U" crossorigin="anonymous"></script>
+</body>
+</html>
