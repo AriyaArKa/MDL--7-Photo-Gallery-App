4334f6c9f32ea9986b7654644757deb3480eb269
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-04-26 21:38:47 +0600

    databse,loaded

---
+++ includes/database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE photo_gallery;
+
+USE photo_gallery;
+
+
+
+-- Table structure for table `photos`
+CREATE TABLE images(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    filename VARCHAR(255) NOT NULL,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
