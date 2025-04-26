f88263ad37206dc46ed55fa40d50481aefee84b4
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-04-26 23:01:36 +0600

    alerts set

--- upload.php
+++ upload.php
@@ -1,36 +1,67 @@
 <?php
 include 'includes/header.php';
+
+
+$error = '';
+$success = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $image = $_FILES['image'];
+
+    if (empty($title) || empty($description)) {
+        $error = "Please fill in all fields";
+    }
+}
 ?>
 
 <div class="container my-4">
-<div class="my-4">
-    <h1>Photo Gallery</h1>
-</div>
+    <div class="my-4">
+        <h1>Photo Gallery</h1>
+    </div>
 
-<div class="row">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-body">
-                <form>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Upload Image</button>
-                </form>
+
+    <?php if ($success) : ?>
+        <div class="alert alert-success" role="alert">
+            <?php echo $success; ?>
+        </div>
+    <?php endif ?>
+
+    <?php if ($error) : ?>
+        <div class="alert alert-danger" role="alert">
+            <?php echo $$error; ?>
+        </div>
+    <?php endif ?>
+
+
+
+
+
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <form method="POST" action="upload.php" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea type="text" class="form-control" name="description"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Select Photo</label>
+                            <input type="file" class="form-control" name="image"></input>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Upload Image</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 </div>
