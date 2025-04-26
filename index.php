088f54392fda354d6fd328ff7e8f21d75ce8dc9e
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-04-27 01:25:15 +0600

    working properly

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 include 'includes/header.php';
-?>
 
+$sql = "SELECT * FROM images ORDER BY upload_date DESC";
+$stmt = $pdo->query($sql);
+
+$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
 
 <div class="container my-4">
   <div class="my-4">
@@ -9,20 +14,34 @@ include 'includes/header.php';
   </div>
 
   <div class="row">
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">2025-23-23</p>
-
+    <?php
+    if (count($images) > 0) {
+      foreach ($images as $image) {
+    ?>
+        <div class="col-md-4 mb-4 d-flex align-items-stretch">
+          <div class="card" style="width: 100%;">
+            <img src="Assets/Images/<?php echo $image['filename']; ?>" class="card-img-top img-fluid" alt="<?php echo $image['title']; ?>" style="height: 200px; object-fit: cover;">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $image['title']; ?></h5>
+              <p class="card-text"><?php echo $image['description']; ?></p>
+              <p class="card-text"><small class="text-muted">Uploaded on <?php echo $image['upload_date']; ?></small></p>
+              <!-- <a href="delete.php?id=<?php echo $image['id']; ?>" class="btn btn-danger">Delete</a>
+              <a href="edit.php?id=<?php echo $image['id']; ?>" class="btn btn-primary">Edit</a> -->
+            </div>
+          </div>
+        </div>
+      <?php
+      }
+    } else {
+      ?>
+      <div class="alert alert-info" role="alert">
+        No images found.
       </div>
-    </div>
+    <?php
+    }
+    ?>
   </div>
-
 </div>
-
-
-
 
 <?php
 include 'includes/footer.php';
