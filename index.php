226410fbcfeb31b3013c9c12805f0c92af50dd49
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-04-26 22:40:38 +0600

    ui issue fixed

--- index.php
+++ index.php
@@ -3,6 +3,26 @@ include 'includes/header.php';
 ?>
 
 
+<div class="container my-4">
+  <div class="my-4">
+    <h1>Photo Gallery</h1>
+  </div>
+
+  <div class="row">
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">2025-23-23</p>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+
 
 <?php
 include 'includes/footer.php';
